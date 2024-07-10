cefc6ca33efa2e78bb79778a7531ccb6c5255bbd
Anubhav Sharan <139759774+AnubhavSharan@users.noreply.github.com>
2024-07-11 00:31:19 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args) {
+        String name="anubhav";
+        int a=3;
+        int b=4;
+        int c=2*(a+b);
+        System.out.println(c);
+        System.out.println(name);
+    }
+    
+}
