2f6d4529b67a0311df527173618c35c97a5252db
Anubhav Sharan <139759774+AnubhavSharan@users.noreply.github.com>
2024-07-11 13:36:29 +0530

    Add files via upload

---
+++ Loop.java
@@ -0,0 +1,21 @@
+// Introduction to Loops
+public class Loop {
+    public static void main(String[] args) {
+        // for(int i=0; i < 4; i=i+1) {
+        //     System.out.println("Hello World!");
+        
+        // int i=0;
+        // while(i<=10){
+        //     System.out.println("My name is Lakhan");
+        //     i++;// i=i+1
+        int i=0;
+        do{
+            System.out.print(i+" ");
+            i++;
+        }while(i<=10);
+
+
+        }
+}
+
+
