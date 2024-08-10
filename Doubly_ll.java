afc6e92c4d4735ee8b47bf3e11b7adb15a4ca048
Anubhav Sharan <139759774+AnubhavSharan@users.noreply.github.com>
2024-08-10 16:02:41 +0530

    Add files via upload

---
+++ Doubly_ll.java
@@ -0,0 +1,17 @@
+package Linked_list;
+
+public class Doubly_ll {
+    public class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public void Insert(int data){
+
+
+    }
+
+}
